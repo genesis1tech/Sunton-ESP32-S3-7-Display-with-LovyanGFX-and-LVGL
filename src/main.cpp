a5af290907d0d79fe685ec99bd0b5f4a2f877499
Marcus Wade <mwade@genesis1.tech>
2024-05-08 23:17:07 -0400

    task setup

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,8 @@
 #include "coke_ad.h"
 #include "pepsi_ad.h"
 #include "coke_ad2.h"
+#include "coke_recycle.h"
+#include "coke_recycle_gen.h"
 
 
 #define SCREEN_W 800
@@ -28,24 +30,55 @@
 #define SCANNER_TX 43
 #define SCANNER_RX 44
 
-const uint16_t* images[] = {coke_ad, tswift_ad, pepsi_ad};
+const uint16_t* images[] = {coke_recycle_gen, coke_ad, tswift_ad, pepsi_ad, coke_ad2, coke_recycle};
 int imageCount = sizeof(images) / sizeof(images[0]);
 
 LGFX lcd;
+LGFX_Sprite sprite(&lcd);
+
+//Tasks
+TaskHandle_t imageTask;
+TaskHandle_t scanTask;
+//SemaphoreHandle_t semaphore;
+
+void imageTaskcode( void * pvParameters ){
+ for (;;) {     
+        for (int i = 0; i < imageCount; i++) {
+            //xSemaphoreTake(semaphore, portMAX_DELAY);
+            lcd.pushImageDMA(0, 0, 800, 480, images[i]);
+            delay(8000); // Display each image for 8 seconds
+            //xSemaphoreGive(semaphore);
+            }
+          }
+  }
 
 void setup(){
   lcd.init();
-  lcd.setBrightness(150);
+  lcd.setBrightness(200);
   lcd.setSwapBytes(true); // Adjust depending on your LCD driver
   SPI.begin(SD_SCK, SD_MISO, SD_MOSI);
+  sprite.createSprite(800, 480);
+  
+  //semaphore = xSemaphoreCreateMutex();
+
+  //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
+  xTaskCreatePinnedToCore(imageTaskcode, "imageTask", 10000, NULL, 1, &imageTask, 0);                  
+  delay(500); 
 }
 
-
+/*
 void loop(){
+lcd.pushImageDMA(0, 0, 800, 480, coke_recycle_gen);
+delay(5000);
+lcd.pushImageDMA(0, 0, 800, 480, coke_ad);
+delay(5000);
 lcd.pushImageDMA(0, 0, 800, 480, tswift_ad);
 delay(5000);
 lcd.pushImageDMA(0, 0, 800, 480, pepsi_ad);
 delay(5000);
 lcd.pushImageDMA(0, 0, 800, 480, coke_ad2);
 delay(5000);
+lcd.pushImageDMA(0, 0, 800, 480, coke_recycle);
+delay(5000);
 }
+*/
